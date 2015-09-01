86916c23e00499685d7c241a363f8b4cd39270d0
EmilyPlatzer <emilyplatzer@gmail.com>
2015-09-01 09:31:00 -0600

    User can add numbers by visiting /add/:term1/:term2

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get("/", function (req, res) {
   res.send("Oh Hai!");
 });
 
+app.get("/add/:term1/:term2", function (req, res) {
+  var sum = Number(req.params.term1) + Number(req.params.term2);
+  res.send(sum.toString());
+})
+
 app.listen(3000, function () {
   console.log("Starting a server on localhost:3000");
 });
