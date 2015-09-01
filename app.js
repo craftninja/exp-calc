8d7be81d83add43978ef124e1a0f6d89b3b99d63
EmilyPlatzer <emilyplatzer@gmail.com>
2015-09-01 09:35:50 -0600

    User can subtract by visiting /subt/:minuend/:subtrahend

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get("/add/:term1/:term2", function (req, res) {
   res.send(sum.toString());
 })
 
+app.get("/subt/:minuend/:subtrahend", function (req, res) {
+  var difference = Number(req.params.minuend) - Number(req.params.subtrahend);
+  res.send(difference.toString());
+})
+
 app.listen(3000, function () {
   console.log("Starting a server on localhost:3000");
 });
